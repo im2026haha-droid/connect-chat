99e06e63ba78bbc3670e34f277936fa32cb298f1
im2026haha-droid <im2026haha-droid@users.noreply.github.com>
2026-07-25 22:20:28 +0900

    fix: 무료 TURN 중계 서버 추가 (NAT 방화벽 우회)

--- script.js
+++ script.js
@@ -17,14 +17,22 @@ const ICE_SERVERS = {
         { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
         { urls: 'stun:stun2.l.google.com:19302' },
-        { urls: 'stun:stun3.l.google.com:19302' },
-        { urls: 'stun:stun4.l.google.com:19302' },
-        { urls: 'stun:stun.ekiga.net' },
-        { urls: 'stun:stun.ideasip.com' },
-        { urls: 'stun:stun.schlund.de' },
-        { urls: 'stun:stun.voiparound.com' },
-        { urls: 'stun:stun.voipstunt.com' },
         { urls: 'stun:stun.services.mozilla.com' },
+        {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
     ],
     iceCandidatePoolSize: 10
 };
